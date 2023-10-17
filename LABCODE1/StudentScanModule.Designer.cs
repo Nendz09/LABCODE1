f8b29127b32392ec8a9b5909b35865d9d24480f6
Noel <105352876+Nendz09@users.noreply.github.com>
2023-10-18 04:08:23 +0800

    meron na tong scanner- walang pang borrowed and returned table

--- LABCODE1/StudentScanModule.Designer.cs
+++ LABCODE1/StudentScanModule.Designer.cs
@@ -30,9 +30,8 @@
         {
             this.cboCam = new System.Windows.Forms.ComboBox();
             this.btnStart = new System.Windows.Forms.Button();
-            this.pictureBox = new System.Windows.Forms.PictureBox();
             this.txt_Barcode = new System.Windows.Forms.TextBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.label_studentName = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // cboCam
@@ -53,35 +52,37 @@
             this.btnStart.UseVisualStyleBackColor = true;
             this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
             // 
-            // pictureBox
-            // 
-            this.pictureBox.Location = new System.Drawing.Point(170, 129);
-            this.pictureBox.Name = "pictureBox";
-            this.pictureBox.Size = new System.Drawing.Size(354, 191);
-            this.pictureBox.TabIndex = 2;
-            this.pictureBox.TabStop = false;
-            // 
             // txt_Barcode
             // 
             this.txt_Barcode.Location = new System.Drawing.Point(170, 381);
             this.txt_Barcode.Name = "txt_Barcode";
             this.txt_Barcode.Size = new System.Drawing.Size(268, 20);
             this.txt_Barcode.TabIndex = 3;
+            this.txt_Barcode.TextChanged += new System.EventHandler(this.txt_Barcode_TextChanged);
+            this.txt_Barcode.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_Barcode_KeyPress);
+            // 
+            // label_studentName
+            // 
+            this.label_studentName.AutoSize = true;
+            this.label_studentName.Location = new System.Drawing.Point(142, 165);
+            this.label_studentName.Name = "label_studentName";
+            this.label_studentName.Size = new System.Drawing.Size(93, 13);
+            this.label_studentName.TabIndex = 4;
+            this.label_studentName.Text = "STUDENT NAME";
             // 
             // StudentScanModule
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label_studentName);
             this.Controls.Add(this.txt_Barcode);
-            this.Controls.Add(this.pictureBox);
             this.Controls.Add(this.btnStart);
             this.Controls.Add(this.cboCam);
             this.Name = "StudentScanModule";
             this.Text = "StudentScanModule";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.StudentScanModule_FormClosing);
             this.Load += new System.EventHandler(this.StudentScanModule_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -91,7 +92,7 @@
 
         private System.Windows.Forms.ComboBox cboCam;
         private System.Windows.Forms.Button btnStart;
-        private System.Windows.Forms.PictureBox pictureBox;
         private System.Windows.Forms.TextBox txt_Barcode;
+        private System.Windows.Forms.Label label_studentName;
     }
 }
--- LABCODE1/StudentScanModule.cs
+++ LABCODE1/StudentScanModule.cs
@@ -1,4 +1,5 @@
 ﻿using AForge.Video.DirectShow;
+using AForge.Video;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -9,11 +10,21 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using ZXing;
+using ZXing.QrCode;
+using ZXing.Common;
+using System.Data.SqlClient;
+using ZXing.PDF417.Internal;
 
 namespace LABCODE1
 {
     public partial class StudentScanModule : Form
     {
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\Admin\Documents\Inventory_Labcode.mdf;Integrated Security=True;Connect Timeout=30");
+        SqlCommand cmd = new SqlCommand();
+        SqlDataReader dr;
+        BarcodeReader reader = new BarcodeReader();
+        
+
         public StudentScanModule()
         {
             InitializeComponent();
@@ -21,34 +32,76 @@ namespace LABCODE1
 
         FilterInfoCollection filterInfoCollection;
         VideoCaptureDevice videoCaptureDevice;
+
         private void StudentScanModule_Load(object sender, EventArgs e)
         {
+            reader.Options = new ZXing.Common.DecodingOptions
+            {
+                PossibleFormats = new List<BarcodeFormat>
+                {
+                    BarcodeFormat.CODABAR,  // STUDENT ID FORMAT
+                    BarcodeFormat.CODE_128,  // code 128
+                    BarcodeFormat.QR_CODE    // qrcode
+                },
+            };
+
             filterInfoCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
-            foreach (FilterInfo device in filterInfoCollection)
-                cboCam.Items.Add(device.Name);
-            cboCam.SelectedIndex = 0;
+            if (filterInfoCollection.Count == 0)
+            {
+                MessageBox.Show("No video capture devices found.");
+            }
+            else
+            {
+                foreach (FilterInfo device in filterInfoCollection)
+                {
+                    cboCam.Items.Add(device.Name);
+                }
+                cboCam.SelectedIndex = 0;
+            }
+            
         }
 
         private void btnStart_Click(object sender, EventArgs e)
         {
+            if (filterInfoCollection.Count == 0)
+            {
+                MessageBox.Show("No video capture devices found.");
+                return;
+            }
+
             videoCaptureDevice = new VideoCaptureDevice(filterInfoCollection[cboCam.SelectedIndex].MonikerString);
             videoCaptureDevice.NewFrame += VideoCaptureDevice_NewFrame;
+            videoCaptureDevice.Start();
         }
 
         private void VideoCaptureDevice_NewFrame(object sender, AForge.Video.NewFrameEventArgs eventArgs)
         {
+
             Bitmap bitmap = (Bitmap)eventArgs.Frame.Clone();
-            BarcodeReader reader = new BarcodeReader();
+            //lalabas ko to
+            //BarcodeReader reader = new BarcodeReader(); 
+
+            //THIS ONE IS PARA MAANO FORMAT NG BARCODE VVVV----------
+            
+
+
             var result = reader.Decode(bitmap);
-            if (result != null) 
+            if (result != null)
             {
                 txt_Barcode.Invoke(new MethodInvoker(delegate ()
                 {
+                    //result.Text siya before
                     txt_Barcode.Text = result.ToString();
                 }));
+                //BarcodeFormat barcodeFormat = result.BarcodeFormat;
             }
-            pictureBox.Image = bitmap;
+            //pictureBox.Image = bitmap;
+
+            //this is pag inverted
+            //reader.AutoRotate = true;
+            //reader.Options.TryInverted = true;
         }
+
 
         private void StudentScanModule_FormClosing(object sender, FormClosingEventArgs e)
         {
@@ -56,6 +109,43 @@ namespace LABCODE1
             {
                 if (videoCaptureDevice.IsRunning)
                     videoCaptureDevice.Stop();
+            }
+        }
+
+        private void txt_Barcode_TextChanged(object sender, EventArgs e)
+        {
+            con.Open();
+
+            if (int.TryParse(txt_Barcode.Text, out int studentID))
+            {
+                cmd = new SqlCommand("SELECT * FROM lab_students WHERE student_id = @studentID", con);
+                cmd.Parameters.AddWithValue("@studentID", studentID);
+
+                dr = cmd.ExecuteReader();
+
+                if (dr.Read())
+                {
+                    label_studentName.Text = dr["full_name"].ToString();
+                }
+                else
+                {
+                    label_studentName.Text = "NO DATA FOUND";
+                }
+                dr.Close();
+            }
+            else
+            {
+                label_studentName.Text = "Invalid Student ID or NOT a number";
+            }
+            con.Close();
+        }
+
+        //TYPING STRING UNAVAILABLE
+        private void txt_Barcode_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
             }
         }
     }
